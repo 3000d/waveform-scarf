2a0d0c1ec6f38291d356e8ac0cbe27c3b30839bf
Jonathan Berger <drskullster@gmail.com>
2013-06-09 20:55:30 +0200

    Added Wavesurfer.js library and index.php

---
+++ generator/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Waveform scarf</title>
+</head>
+<body>
+<header>
+    <h1>Waveform scarf</h1>
+    <h2>Generate ready-to-knit image of your favorite track.</h2>
+</header>
+<div id="content">
+    <div id="drop_zone">
+        Drop your files here
+    </div>
+    <div id="waveform_zone">
+        <p>
+            Your scarf here :
+        </p>
+        <canvas id="generated">
+
+        </canvas>
+    </div>
+</div>
+<footer>
+    Hell yeah.
+</footer>
+</body>
+</html>
